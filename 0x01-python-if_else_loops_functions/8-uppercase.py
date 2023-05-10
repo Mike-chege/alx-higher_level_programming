7eb536c894ae4cb4526500971b471572128ff658
mikechege01 <mike@mail.com>
2023-05-10 12:50:17 -0700

    uppercase Task fixed typos

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(s):
     result = ""
     for c in s:
-        if ord(c) >+ 97 and ord(c) <= 122:
+        if ord(c) >= 97 and ord(c) <= 122:
             result += "{}".format(chr(ord(c) - 32))
         else:
             result += "{}".format(c)
-    print("{}".format(result), end="")
+    print("{}\n".format(result), end="")
