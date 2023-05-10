8c25482f8c0eda4e608d2f3cffe828915399ab88
mikechege01 <mike@mail.com>
2023-05-10 13:59:17 -0700

    Insert in sorted linekd list Task update

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,26 +9,26 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *current_node;
 
-	new_node =  malloc(sizeof(listint_t));
-	if(new_node == NULL)
+	listint_t *current_node = *head, *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = number;
-	new_node->next = NULL;
 
-	if(*head == NULL || number < (*head)->n)
+	if (current_node == NULL || current_node->n >= number)
 	{
-		new_node->next = *head;
+		new_node->next = current_node;
 		*head = new_node;
 		return (new_node);
 	}
-	current_node = *head;
-	while(current_node->next != NULL && current_node->next->n < number)
+
+	while (current_node && current_node->next && current_node->next->n < number)
 		current_node = current_node->next;
 
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 
-	return(new_node);
+	return (new_node);
 }
