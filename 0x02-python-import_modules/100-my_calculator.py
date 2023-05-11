d70b51465fad363bdff32d6a821c77e2716afff1
mikechege01 <mike@mail.com>
2023-05-11 05:48:14 -0700

    Task [100-my_calculator.py] - [Build calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
     import sys
 
@@ -8,11 +7,11 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    my_op = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(my_op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    print("{:d} {:s} {:d} = {:d}".format(a, sys.argv[2], b, my_op[sys.argv[2]](a, b)))
