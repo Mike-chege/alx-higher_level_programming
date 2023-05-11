73e77ec1755580740b35fb294dd78577f250a75d
mikechege01 <mike@mail.com>
2023-05-11 04:47:00 -0700

    Task [4-hidden_discovery.py] - [Who are you ?]

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
+    """Output is the names in hidden_4 module."""
     import hidden_4 as hidden
 
     for name in dir(hidden):
