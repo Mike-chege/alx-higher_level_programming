d4a4687f31d866926405046faf263d71084ada02
mikechege01 <mike@mail.com>
2023-05-11 04:24:49 -0700

    Task[4-hidden_discovery.py]

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import py
+    import sys
+
+    py_compile.compile("hidden_4py")
+
+    import hidden_4
+
+    names = dir(hidden_4)
+
+    for name in sorted(names):
+        if not name.startwith("__"):
+            print(name)
