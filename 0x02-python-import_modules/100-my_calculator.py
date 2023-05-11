8410f5df4bcd45209d60374b6c200bdb2c792b54
mikechege01 <mike@mail.com>
2023-05-11 05:21:34 -0700

    Fixed typos at print[functio]

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{:d} {:d} {:d} = {:d}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
+    print("{:d} {} {:d} = {:d}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
