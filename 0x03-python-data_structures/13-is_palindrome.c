272cfccfa3e9c184b77a81d236a7964d00f711ed
mikechege01 <mike@mail.com>
2023-05-15 13:19:44 -0700

    palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,19 +2,17 @@
 #include <stdio.h>
 
 void reverse_list(listint_t **head);
-int list_equiv(listint_t *l1, listint_t *l2);
+int list_identical(listint_t *l1, listint_t *l2);
 
 /**
  * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the head of the linked list
- *
- * Return: 0 (not palindrome) 1 (is palindrome)
+ * @head: pointer to the head of the linked list
+ * Return: 0 if not palindrome 1 if palindrome
  */
 int is_palindrome(listint_t **head)
 {
 	listint_t *skip_1, *skip_2, *prev_s1, *first_half, *second_half, *mid;
 
-	/* linked lists of length 0 and 1 are palindromes by default */
 	if (!head || !(*head) || !((*head)->next))
 		return (1);
 
@@ -27,31 +25,29 @@ int is_palindrome(listint_t **head)
 		skip_1 = skip_1->next;
 		skip_2 = skip_2->next->next;
 	}
-	if (skip_2 == NULL) /* Even # of nodes */
+	if (skip_2 == NULL)
 		second_half = skip_1;
-	else /* odd number of nodes, there is a middle node */
+	else
 	{
 		mid = skip_1;
 		second_half = skip_1->next;
 	}
-	prev_s1->next = NULL; /* null terminate first half */
+	prev_s1->next = NULL;
 	reverse_list(&second_half);
 
 	if (list_equiv(first_half, second_half))
-		return (1); /* equivalent lists, palindrome found */
+		return (1);
 	else
 		return (0);
 }
 
 /**
- * list_equiv - checks if two linked lists contain identical data and are
- * the same length as each other
+ * list_identical - checks if two linked lists contain identical elements
  * @l1: list one to compare to list two
  * @l2: list two to compare to list one
- *
- * Return: 1 (equivalent) 0 (not equal)
+ * Return: 1 if identical, 0 if not dentical
  */
-int list_equiv(listint_t *l1, listint_t *l2)
+int list_identical(listint_t *l1, listint_t *l2)
 {
 	while (l1 || l2)
 	{
@@ -67,21 +63,20 @@ int list_equiv(listint_t *l1, listint_t *l2)
 
 /**
  * reverse_list - reverses a linked list
- * @head: double pointer to head of linked list so we can modify it
- *
- * Return: always void, modifies head itself.
+ * @head: pointer to head of linked list
+ * Return: Nothing
  */
 void reverse_list(listint_t **head)
 {
-	listint_t *next = NULL, *prev = NULL, *cur;
+	listint_t *next = NULL, *prev = NULL, *new;
 
-	cur = *head;
-	while (cur)
+	new = *head;
+	while (new)
 	{
-		next = cur->next;
-		cur->next = prev;
-		prev = cur;
-		cur = next;
+		next = new->next;
+		new->next = prev;
+		prev = new;
+		new= next;
 	}
 	*head = prev;
 }
