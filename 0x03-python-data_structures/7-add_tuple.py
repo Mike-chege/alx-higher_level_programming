a99a2b492203c299fd0cca1f73a43986404e3d0e
mikechege01 <mike@mail.com>
2023-05-15 11:14:33 -0700

    Task[7-add_tuple.py] adds two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Task 7
+
+def add_tuple(tuple=(), tuple_b=()):
+    """
+    adds two tuples
+    """
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])
