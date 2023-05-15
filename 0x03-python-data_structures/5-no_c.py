8f41cd93fee54e493ba36b6d5f0e9892e7d3a1b9
mikechege01 <mike@mail.com>
2023-05-15 10:09:46 -0700

    Task[5-no_c.py] removes all charcters 'c' and 'C' from a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Task 5
+
+def no_c(my_string):
+    """removes all characters 'c' and 'C' from a string"""
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char 'C':
+            new_string += char
+    return new_string
