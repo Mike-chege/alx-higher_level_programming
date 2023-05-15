e5a1659ba602e063eb227c0dcf94cc22b5e7b17d
mikechege01 <mike@mail.com>
2023-05-15 13:36:54 -0700

    palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void reverse_list(listint_t **head);
-int list_identical(listint_t *l1, listint_t *l2);
+int list_identical(listint_t *list1, listint_t *list2);
 
 /**
  * is_palindrome - checks if a linked list is a palindrome
@@ -11,31 +12,31 @@ int list_identical(listint_t *l1, listint_t *l2);
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *skip_1, *skip_2, *prev_s1, *first_half, *second_half, *mid;
+	listint_t *first_1, *second_2, *prev_l, *first_top, *second_top, *middle;
 
 	if (!head || !(*head) || !((*head)->next))
 		return (1);
 
-	first_half = skip_1 = skip_2 = prev_s1 = *head;
-	second_half = mid = NULL;
+	first_top = first_1 = second_2 = prev_l = *head;
+	second_top = middle = NULL;
 
-	while (skip_1 && skip_2 && skip_2->next)
+	while (first_1 && second_2 && second_2->next)
 	{
-		prev_s1 = skip_1;
-		skip_1 = skip_1->next;
-		skip_2 = skip_2->next->next;
+		prev_l = first_1;
+		first_1 = first_1->next;
+		second_2 = second_2->next->next;
 	}
-	if (skip_2 == NULL)
-		second_half = skip_1;
+	if (second_2 == NULL)
+		second_top = first_1;
 	else
 	{
-		mid = skip_1;
-		second_half = skip_1->next;
+		middle = first_1;
+		second_top = first_1->next;
 	}
-	prev_s1->next = NULL;
-	reverse_list(&second_half);
+	prev_l->next = NULL;
+	reverse_list(&second_top);
 
-	if (list_identical(first_half, second_half))
+	if (list_identical(first_top, second_top))
 		return (1);
 	else
 		return (0);
@@ -43,20 +44,20 @@ int is_palindrome(listint_t **head)
 
 /**
  * list_identical - checks if two linked lists contain identical elements
- * @l1: list one to compare to list two
- * @l2: list two to compare to list one
+ * @list1: compares to list two
+ * @list2: compares to list one
  * Return: 1 if identical, 0 if not dentical
  */
-int list_identical(listint_t *l1, listint_t *l2)
+int list_identical(listint_t *list1, listint_t *list2)
 {
-	while (l1 || l2)
+	while (list1 || list2)
 	{
-		if (l1->n != l2->n || !l1 || !l2)
+		if (list1->n != list2->n || !list1 || !list2)
 			return (0);
-		if (l1)
-			l1 = l1->next;
-		if (l2)
-			l2 = l2->next;
+		if (list1)
+			list1 = list1->next;
+		if (list2)
+			list2 = list2->next;
 	}
 	return (1);
 }
