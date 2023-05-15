3e08ccf37189c7c237f7ae96bd5a90479ab2d8e8
mikechege01 <mike@mail.com>
2023-05-15 11:25:07 -0700

    Task[8-multiple_returns.py] returns a tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Task 8
+
+def multiple_returns(sentence):
+    """
+    returns tuple with the length of a string and its character
+    """
+    if len(sentence) == 0:
+        return (0, None)
+    else:
+        return (len(sentences), sentence[0])
