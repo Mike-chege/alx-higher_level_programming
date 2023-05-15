929e09663ac453e3b2d2a76b12a337ecb5a81882
mikechege01 <mike@mail.com>
2023-05-15 11:57:50 -0700

    Task[11-delete_at.py] deletes function using del statement/fixed typos

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,9 +3,11 @@
 
 def delete_at(my_list=[], idx=0):
     """
-    deletes the item at a specific position in a list
+    deletes the item at a specific position in a list and returns
+    modified list
     """
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        return my_list[:idx] + my_list[idx+1:]
+        del my_list[idx]
+        return my_list
