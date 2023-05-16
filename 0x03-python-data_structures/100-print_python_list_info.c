f7e34ecdd657ac6e545025bd49796de8a3286319
mikechege01 <mike@mail.com>
2023-05-16 04:41:07 -0700

    Task[print_list.c] fixed typos

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,10 +8,10 @@
  */
 void print_python_list_info(PyObject *p)
 {
+	const char *e_kind = NULL;
 	PyListObject *list_p = NULL;
 	ssize_t size = 0;
 	ssize_t i = 0;
-	const char *e_kind = NULL;
 
 	size = PyList_Size(p);
 	list_p = (PyListObject *)p;
@@ -19,7 +19,7 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Allocated = %ld\n", (signed long)(list_p->allocated));
 	while (i < size)
 	{
-		e_kind = Py_TYPE(list_p->ob_item[i])->tp_name;
+		e_kind = Py_TYPE(list_p->ob_t[i])->tmp_n;
 		printf("Element %ld: %s\n", i, e_kind);
 		i++;
 	}
