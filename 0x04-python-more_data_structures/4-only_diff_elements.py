573095065edd234b7062b4e2141898520d24e1ad
mikechege01 <mike@mail.com>
2023-05-17 12:59:27 -0700

    Task[diff_elements.py]

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Task 4
+
+def only_diff_elements(set_1, set_2):
+    """
+    find all elements present in only one set and add the 
+    to the set
+    """
+    only_diff = set()
+    for elem in set_1:
+        if elem not in set_2:
+            only_dif.add(elem)
+    for elem in set_2:
+        if elem not in set_1:
+            only_diff.add(elem)
+
+    return only_diff
