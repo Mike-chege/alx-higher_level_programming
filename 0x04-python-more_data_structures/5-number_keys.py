de6d3803d71cd43c359cda73f4d0e71be6375e8b
mikechege01 <mike@mail.com>
2023-05-17 13:06:55 -0700

    Task[number_keys.py]

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Task 5
+
+def number_keys(a_dictionary):
+    """
+    count the number of keys in the dictionary
+    """
+    count = 0
+    for key in a_dictionary:
+        count += 1
+
+    return count
