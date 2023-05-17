2db680cd1b4253ef880b66bba79590c4482a3e24
mikechege01 <mike@mail.com>
2023-05-17 12:33:53 -0700

    Task[replace_element.py]

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Task 2
+
+def search_replace(my_list, search, replace):
+    """Replace all occurrences of the search element with the replace element"""
+    result = [elem for elem in my_list]
+    for i in range(len(result)):
+        if result[i] == search:
+            result[i] = replace
+
+            return result
