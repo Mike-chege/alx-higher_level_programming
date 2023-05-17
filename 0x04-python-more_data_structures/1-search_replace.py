9cbae3ff5f71e902cea9234ebc221daf09dfdc91
mikechege01 <mike@mail.com>
2023-05-17 12:40:04 -0700

    Task[replace_element.py] fixed pycodestyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,9 @@
 # Task 2
 
 def search_replace(my_list, search, replace):
-    """Replace all occurrences of the search element with the replace element"""
+    """
+    replace all occurrences of the search element with the replace element
+    """
     result = [elem for elem in my_list]
     for i in range(len(result)):
         if result[i] == search:
