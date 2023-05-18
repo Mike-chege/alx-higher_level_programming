593f6b75a545f4407c3863ad42f8e509c0351007
mikechege01 <mike@mail.com>
2023-05-18 02:45:29 -0700

    Task[best_key.py]

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,9 +7,10 @@ def best_score(a_dictionary):
     """
     best_key = None
     best_value = 0
-    for key, value in a_dictionary.items():
-        if isinstance(a_dictionary, dict) and value  > best_value:
-            best_key = key
-            best_value = value
+    if isinstance(a_dictionary, dict):
+        for key, value in a_dictionary.items():
+            if value > best_value:
+                best_key = key
+                best_value = value
 
     return best_key
