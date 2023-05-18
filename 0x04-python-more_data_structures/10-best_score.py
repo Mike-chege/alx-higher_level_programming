7fff6bbe920c4381cb9f14a8b14d7bea9574827f
mikechege01 <mike@mail.com>
2023-05-18 02:20:03 -0700

    Task[best_score.py] fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -11,6 +11,6 @@ def best_score(a_dictionary):
         if isinstance(a_dictionary.get(key), int) and \
                 a_dictionary.get(key) > best_value:
             best_key = key
-            best_value = a_dictionary.get[key]
+            best_value = a_dictionary[key]
 
     return best_key
