71ba8e0f930236de7082f297ee7c31bbf1ed7fdf
mikechege01 <mike@mail.com>
2023-05-18 04:46:32 -0700

    Task[103-python.c] cpython/PythonByteObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -12,17 +12,17 @@ void print_python_list(PyObject *p)
 {
 	PyListObject *list = (PyListObject *)p;
 	PyVarObject *var = (PyVarObject *)p;
-	int size, alloc, i;
+	int length, allocation, i;
 	const char *type;
 
-	size = var->ob_size;
-	alloc = list->allocated;
+	length = var->ob_size;
+	allocation = list->allocated;
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d\n", alloc);
+	printf("[*] Size of the Python List = %d\n", length);
+	printf("[*] Allocated = %d\n", allocation);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < length; i++)
 	{
 		type = list->ob_item[i]->ob_type->tp_name;
 		printf("Element %d: %s\n", i, type);
@@ -38,7 +38,7 @@ void print_python_list(PyObject *p)
 void print_python_bytes(PyObject *p)
 {
 	PyBytesObject *bytes = (PyBytesObject *)p;
-	unsigned char i, size;
+	unsigned char i, length;
 
 	printf("[.] bytes object info\n");
 	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
@@ -51,15 +51,15 @@ void print_python_bytes(PyObject *p)
 	printf("  trying string: %s\n", bytes->ob_sval);
 
 	if (((PyVarObject *)p)->ob_size > 10)
-		size = 10;
+		length = 10;
 	else
-		size = ((PyVarObject *)p)->ob_size + 1;
+		length = ((PyVarObject *)p)->ob_size + 1;
 
-	printf("  first %d bytes: ", size);
-	for (i = 0; i < size; i++)
+	printf("  first %d bytes: ", length);
+	for (i = 0; i < length; i++)
 	{
 		printf("%02hhx", bytes->ob_sval[i]);
-		if (i == (size - 1))
+		if (i == (length - 1))
 			printf("\n");
 		else
 			printf(" ");
