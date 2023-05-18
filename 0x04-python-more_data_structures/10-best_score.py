b5c062e13cf7a4d7039da9571eb286c0fee9cb34
mikechege01 <mike@mail.com>
2023-05-18 01:51:12 -0700

    best_ket.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,9 +8,9 @@ def best_score(a_dictionary):
     best_key = None
     best_value = float('-inf')
     for key in a_dictionary:
-        if isinstance(a_dictionary[key], int) and a_dictionary.get[key] > best_value:
+        if isinstance(a_dictionary.get(key), int) and a_dictionary.get(key) > best_value:
             best_key = key
-            best_value = a_dictionary[key]
+            best_value = a_dictionary.get(key)
 
     return best_key
 
