6f4229921a18b5eb20d30b2205458ce81ff33e63
mikechege01 <mike@mail.com>
2023-05-18 01:11:56 -0700

    Task[update_dictionary.py]

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Task 7
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    update or add the key-value pair in the dictionary
+    """
+    a_dictionary[key] = value
+
+    return a_dictionary
