ef96b95acdde3bc1d06eb94298ea6c656c0fed70
mikechege01 <mike@mail.com>
2023-05-18 02:57:50 -0700

    Task[multiply_list_map.py] reduced number of line

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-# Task 11
-
 def multiply_list_map(my_list=[], number=0):
-    """use map to multiply each value in the list by the number"""
     return list(map(lambda x: x * number, my_list))
