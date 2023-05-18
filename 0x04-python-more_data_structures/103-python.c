c15daf196cb2349bd6c3a4b62455558a1a8521e8
mikechege01 <mike@mail.com>
2023-05-18 04:00:16 -0700

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,8 @@
 #include <Python.h>
 #include <stdio.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
 /**
  * print_python_list - Prints some basic info about Python lists
  * @p: A pointer to a Python list object
