1d408d8fedd9173eee91fc4eeb9840112666f874
mikechege01 <mike@mail.com>
2023-05-18 01:57:26 -0700

    Task[best_score.py] fixed pycodestyle/split lines

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,14 +3,14 @@
 
 def best_score(a_dictionary):
     """
-    find the key with the biggest integer
+    Returns the key with the biggest integer
     """
     best_key = None
     best_value = float('-inf')
     for key in a_dictionary:
-        if isinstance(a_dictionary.get(key), int) and a_dictionary.get(key) > best_value:
+        if isinstance(a_dictionary.get(key), int) and \
+                a_dictionary.get(key) > best_value:
             best_key = key
             best_value = a_dictionary.get(key)
 
     return best_key
-
