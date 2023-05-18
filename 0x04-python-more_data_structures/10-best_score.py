65263e3186f80074a4ef57368242392c3cedd799
mikechege01 <mike@mail.com>
2023-05-18 01:31:58 -0700

    Task[best_score.py] biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Task 10
+
+def best_score(a_dictionary):
+    """
+    find the key with the biggest integer value
+    """
+    best_key = None
+    best_value = float('-inf')
+    for key in a_dictionary:
+        if isinstance(a_dictionary[key], int) and a_dictionary[key] > best_value:
+            best_key = key
+            best_value = a_dictionary[key]
+
+    return best_key
+
