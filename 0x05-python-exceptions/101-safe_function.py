2e5c6717e48065bf22683ba5ad79a533e9fd7863
mikechege01 <mike@mail.com>
2023-05-29 12:51:46 -0700

    Task[safe_function.py] fixed typos

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -15,6 +15,6 @@ def safe_function(fct, *args):
     try:
         count = fct(*args)
         return count
-    except (None): 
+    except: 
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
