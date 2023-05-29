4cb39d9e9a901c6925ce8e975b944c54b5807f6b
mikechege01 <mike@mail.com>
2023-05-29 06:06:59 -0700

    Task[safe_print.py]

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,12 +6,12 @@ def safe_print_list(my_list=[] x=0):
     while i < x:
         try:
             if isinstance(my_list[i], (int, str)):
-                print(my_list[i], end=' ')
+                print("{}".format(my_list[i]), end="")
                 count += 1
             else:
                 print("Error: Element is not an integer or string")
         except IndexError:
             break
         i += 1
-    print('\n')
+    print("\n")
     return count
