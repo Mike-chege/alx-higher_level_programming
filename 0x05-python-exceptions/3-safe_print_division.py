8bdeec853ab2f62cfc8a1c8960b8d699f7de653d
mikechege01 <mike@mail.com>
2023-05-29 11:36:03 -0700

    Task[safe_print_division.py] division of two integers

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Task 3
+
+def safe_print_division(a, b):
+    """
+    A function that divides two integers and prints the result
+    """
+    try:
+        result = a / b
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return(result)
