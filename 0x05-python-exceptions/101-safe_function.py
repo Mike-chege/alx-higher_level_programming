6b34f5dc9350a39561786c2b622014098559524b
mikechege01 <mike@mail.com>
2023-05-29 12:44:16 -0700

    Task[safe_function.py] excecutes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Task 8
+
+def safe_function(fct, *args):
+    """
+    This function excecutes a function safely
+    and
+    Returns the result of the function(count)
+    otherwise
+    None
+    """
+    try:
+        count = fct(*args)
+        return count
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
