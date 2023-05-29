78f7cd9a3ad8fcabb9ae90b8c1196a982be405db
mikechege01 <mike@mail.com>
2023-05-29 11:08:06 -0700

    Task[1-safe_print_innteger.py] integer lists

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Task 1
+
+def safe_print_integer(value):
+    """
+    prints an integer with "{:d}".format()
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError):
+        return False
