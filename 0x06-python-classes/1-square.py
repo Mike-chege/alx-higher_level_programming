5585ccd3b2f1f238049327ecb0c958be9b1a4e81
mikechege01 <mike@mail.com>
2023-05-31 11:06:55 -0700

    Instantiation with size (no type/value verification)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Task 1
+"""defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """
+    class attributes:
+    """
+    def __init__(self, size):
+        """
+        initializating function
+        """
+        self.__size = size
