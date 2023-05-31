dce0bf1447efd8178a18ee2f2c4c9524f0d12227
mikechege01 <mike@mail.com>
2023-05-31 11:13:59 -0700

    Private instance attribute size typo fixed

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Task 2
+"""Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+    """
+    class attributes
+    """
+    def __init__(self, size=0):
+        """
+        initializing functions for the square class
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
