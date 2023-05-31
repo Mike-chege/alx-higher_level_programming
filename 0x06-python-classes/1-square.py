a571e07a48fd50b6eeb6ca1217ba6976511676a0
mikechege01 <mike@mail.com>
2023-05-31 07:24:15 -0700

    Task[square_size.py] privite instance atrribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Task 1
+
+
+class Square:
+    """
+    class square that defines a square
+    """
+    def __init__(self, size):
+        """
+        Initializing a new square
+        """
+        self.__size = size
