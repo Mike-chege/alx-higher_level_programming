55dd0c20ca2d79ce60171cecfe65c4db7ebffe33
mikechege01 <mike@mail.com>
2023-06-12 06:51:20 -0700

    checking if object is instace of class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Task 2
+"""A function that returns True or Falsse"""
+
+
+def is_name_class(obj, a_class):
+    """Returns True or False depending on the output"""
+    if not isinstance(a_class, type):
+        raise TypeError("a_class must be of type 'type'")
+    return (type(obj) is a_class)
