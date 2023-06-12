4ee9809e020976b44b825973ac7574a5c3cb2188
mikechege01 <mike@mail.com>
2023-06-12 11:36:37 -0700

    Task[11-square.py] inherits from Rectangle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,17 +6,20 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """square shape class, super class is BaseGeometry, then Rectangle
+    """
+    class square inheriting Rectangle
     """
     def __init__(self, size):
-        """instantiation method for class
+        """
+        instantiation method for class
         """
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
     def __str__(self):
-        """overide magic str method for class
+        """
+        overide str method for class
         """
         string = "[Square] " + str(self.__size) + '/'
         string += str(self.__size)
