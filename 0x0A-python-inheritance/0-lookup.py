5b52ed7a202a1ef6361c2187905474dc49bce933
mikechege01 <mike@mail.com>
2023-06-12 06:14:03 -0700

    Returning a list of available objects and attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,6 +5,4 @@
 
 def lookup(obj):
     """Returns a list of available attributes and methods"""
-    attributes = [attr for attr in dir(obj) if not attr.startswith("__")]
-    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
-    return sorted(attributes + methods)
+    return (dir(obj))
