83261242e6f957474da3fa13a7f26abbad3769a8
mikechege01 <mike@mail.com>
2023-06-12 07:27:02 -0700

    Raising an exception error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Task 6
+"""A class BaseGeometry (based on 5-base_geometry.py)"""
+
+
+def area(self):
+    """
+    raises an Exception with a message
+    area() is not implemented"""
+    raise Exception("area() is not implemented")
