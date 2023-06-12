ec42c7bda3eb58b5ef7e8545acc95b79b03ba71d
mikechege01 <mike@mail.com>
2023-06-12 07:01:11 -0700

    Task [2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 # Task 2
-"""A function that returns True or False"""
+"""A function that checks if an object is an instance a class"""
 
 
 def is_name_class(obj, a_class):
-    """Returns True or False depending on the output"""
+    """
+    checking if an object is exactly an instance of
+    a class"""
     if type(obj) == a_class:
         return True
     return False
