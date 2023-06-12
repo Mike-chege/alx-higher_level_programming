0d8141b63c346aa06edafea4f862750f3c79b53d
mikechege01 <mike@mail.com>
2023-06-12 08:50:39 -0700

    Inheriting class from 9-rectangle.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,4 +12,4 @@ class Square(Rectangle):
     def __init__(self, size):
         super().__init__(size, size)
         self.integer_validator("size", size)
-        self.__size =
+        self.__size = size
