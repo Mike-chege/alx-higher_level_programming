babfddd451110b15d9a84cd84d3e4968e5fcfc27
mikechege01 <mike@mail.com>
2023-06-14 03:46:15 -0700

    Fixed typos cousing reload fail

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,12 +10,13 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-     def to_json(self, attrs=None):
+    def to_json(self, attrs=None):
          """Retrieving a dictionary representation of student"""
          if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
     def reload_from_json(self, json):
         """Replacing all attributes of the student instance"""
         for k, v in json.items():
