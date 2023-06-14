cca6f025fde8239a023badf5e94fdc9e0b5f8b3b
mikechege01 <mike@mail.com>
2023-06-14 03:03:12 -0700

    Fixed bare except / typos

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # Task 7
 """adds all arguments to a Python list, and then saves them to a file"""
-
-
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -14,7 +14,7 @@ def main():
     """
     try:
         new_list = load_from_json_file('add_item.json')
-    except:  # file didn't exist to read from
+    except NameError:
         new_list = []
 
     new_list.extend([sys.argv[i] for i in range(0, len(sys.argv)) if i != 0])
@@ -22,6 +22,7 @@ def main():
         save_to_json_file(new_list, 'add_item.json')
     except NameError:
         print("no such file or directory")
+
 
 main()
 
