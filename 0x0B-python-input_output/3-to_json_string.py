cc9e100941b088cf48ae303b1124e81cfd501586
mikechege01 <mike@mail.com>
2023-06-14 02:13:20 -0700

    Returning JSON representation of an object(string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Task 3
+"""Returns JSON representation of an obect(string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns json rep of the object(string)"""
+    return json.loads(my_obj)
