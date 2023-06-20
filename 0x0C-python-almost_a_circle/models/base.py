896c9d02ae8866020361543cb50d51678b78f67a
mikechege01 <mike@mail.com>
2023-06-20 13:53:51 -0700

    Fixed create method to use update method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,11 +16,11 @@ class Base:
         """
         instantiates the base class
         """
-        if id is None:
-            self.id = Base.__nb_objects + 1
-            Base.__nb_objects += 1
-        else:
+         if id is not None:
             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
