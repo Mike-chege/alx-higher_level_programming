49df202c84b171b088bc5937d9727d7151973691
mikechege01 <mikechege93@gmail.com>
2023-07-19 10:08:50 -0700

    Listing all privileges of the MySQL users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- This script lists all privileges
+-- Of the MySQL users user_0d_1 and
+-- user_0d_2 (in localhost)
+SHOW GRANTS FOR "user_0d_1"@"localhost";
+SHOW GRANTS FOR "user_0d_2"@"localhost";
