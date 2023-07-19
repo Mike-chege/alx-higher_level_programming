2a2023fa5aee9bf9c314b67d04e15e439744e947
Mike-chege <mikechege93@gmail.com>
2023-07-19 16:28:25 -0700

    Listing all commedy shows in the database/fixed typos

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- This script lists all comedy shows
 -- In the database hbtn_0d_tvshows
 SELECT tv_shows.title AS "title" FROM tv_show_genres INNER JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows_id INNER JOIN tv_genres
+ON tv_show_genres.show_id = tv_shows.id INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 AND tv_genres.name = "Comedy"
 ORDER BY title;
