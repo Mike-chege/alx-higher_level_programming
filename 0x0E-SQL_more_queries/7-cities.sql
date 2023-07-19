6a95d5c1679cca64be8ee0bfa471b04c90139066
mikechege01 <mikechege93@gmail.com>
2023-07-19 11:22:47 -0700

    Fixed typos the FOREIGN KEY now references to id of the states

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
 -- On MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hhbt_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT NOT NULL AUTO_INCREMENT UNIQUE,
 	state_id INT NOT NULL,
