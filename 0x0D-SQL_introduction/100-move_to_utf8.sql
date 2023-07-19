51d30ecd47d4cdc4ca83814eb9dc6fd86b9bac84
mikechege01 <mikechege93@gmail.com>
2023-07-19 04:21:22 -0700

    Converts hbtn_0c_0 database to UTF8 using table first_table/fixed typos

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- This script converts hbtn_0c_0 database to UTF8
 -- In MYSQL server
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4, COLLATE utf8mb4_unicode_ci;
-ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name BLOB;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SHOW FULL COLUMNS FROM first_table;
