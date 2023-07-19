d047e336c59747238c22a9005eda6247ec48d18f
mikechege01 <mikechege93@gmail.com>
2023-07-19 10:11:26 -0700

    Listing all privileges of the MySQL users/fixed typos

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- This script lists all privileges
 -- Of the MySQL users user_0d_1 and
 -- user_0d_2 (in localhost)
-SHOW GRANTS FOR "user_0d_1"@"localhost";
-SHOW GRANTS FOR "user_0d_2"@"localhost";
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
