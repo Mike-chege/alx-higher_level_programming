0f6a2785f9d6a5f7dcc1bcb47ea923a2e51e85b6
mikechege01 <mikechege93@gmail.com>
2023-07-19 10:40:29 -0700

    Fixed typos / added comments

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 -- This script creates the MySQL server
 -- user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Grant privilleges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
--- Flush the privilleges
-FLUSH PRIVILEGES;
+-- Grant permission after creation to the user
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
