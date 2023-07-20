a1c4ff8e52a62bac3d3c9e03b168d0afc7f98ffc
Mike-chege <mikechege93@gmail.com>
2023-07-20 04:46:08 -0700

    Rating tv shows in a descending order

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,5 @@
+-- This script lists all shows from the database
+-- By their rating
+SELECT title, SUM(rate) AS rating FROM tv_shows AS t
+INNER JOIN tv_show_ratings AS r ON t.id = r.show_id
+GROUP BY title ORDER BY rating DESC;
