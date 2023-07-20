eb53badb32e0d8c4ca454b5192d0e6c398b94de9
Mike-chege <mikechege93@gmail.com>
2023-07-20 04:40:55 -0700

    Displaying all tvshows without the genre comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,5 @@
 -- This script lists all tvshows from the database
--- By their rating
+-- Which are not comedy
 SELECT DISTINCT title FROM tv_shows AS t
        LEFT JOIN tv_show_genres AS s ON s.show_id = t.id
        LEFT JOIN tv_genres AS g ON g.id = s.genre_id
