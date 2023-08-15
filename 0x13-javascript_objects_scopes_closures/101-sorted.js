9ecbf6a69a5ebd8264c72bf0c99a1d679aeccce9
Mike-chege <mikechege93@gmail.com>
2023-08-15 13:17:51 -0700

    Importing a dictionary occurences by user id

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+ * This script imports a dictionary of occurrences by user id
+ * and computes a dictionary of user ids by occurrence
+ */
+const dict = require('./101-data.js').dict;
+let myDict = {};
+for (let key in dict) {
+  if (myDict[dict[key]] === undefined) {
+    myDict[dict[key]] = [key];
+  } else {
+    myDict[dict[key]].push(key);
+  }
+}
+// Printing the new dictionary
+console.log(myDict);
