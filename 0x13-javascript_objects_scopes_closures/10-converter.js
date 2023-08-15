74ce04d30202f8de1fd087df92091983e369a15b
Mike-chege <mikechege93@gmail.com>
2023-08-15 13:02:06 -0700

    Passing a number as an argument then converting it to base 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,4 +2,4 @@
 /* This function converts a number from
  * base 10 to another base passed as argument
  */
-exports.converter = function (base) { return num => num.toString(base); };
+exports.converter = function (base) { return val => val.toString(base); };
