c50610d229c71752e4e3a3915a9c3740694a451c
Mike-chege <mikechege93@gmail.com>
2023-08-15 08:26:54 -0700

    Using For loop to print three lines like the previous task/Fixed typos

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // Using for loop to print 3 lines like previous 1-multi_languages.js
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(line);
 }
