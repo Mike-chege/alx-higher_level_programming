fdcc8b713939401433ee3e69ad4496bf1a665962
Mike-chege <mikechege93@gmail.com>
2023-08-15 12:49:47 -0700

    Implementing a function that prints already printed args in numbers/fixed styling

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -3,4 +3,4 @@
  * already printed and the new argument value
  */
 let num = 0;
-exports.logMe = function (item) { console.log(${num++}: ${item}); }
+exports.logMe = function (item) { console.log(`${num++}: ${item`}); }
