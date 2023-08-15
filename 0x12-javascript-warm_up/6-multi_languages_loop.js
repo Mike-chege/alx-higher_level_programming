557a0c40812f87aa8513cbb28a648ba18e8251f7
Mike-chege <mikechege93@gmail.com>
2023-08-15 08:25:10 -0700

    Using For loop to print three lines like the previous task

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Using for loop to print 3 lines like previous 1-multi_languages.js
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
+}
