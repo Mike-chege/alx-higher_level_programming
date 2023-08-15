7d001ea44536f0d7a3fef327aef390d4995a9399
Mike-chege <mikechege93@gmail.com>
2023-08-15 08:42:35 -0700

    Using for again to print the string C is fun x number of times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+
+const arg = process.argv[2];
+
+if (!isNaN(arg)) {
+  const x = parseInt(arg);
+
+  if (x >= 0) {
+    for (let i = 0; i < x; i++) {
+      console.log('C is fun');
+    }
+  } else {
+    console.log('Number of occurrences must be non-negative');
   }
+} else {
+  console.log('Missing number of occurrences');
 }
