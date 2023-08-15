e6e154848fac02a8070450bd5aef2aba6423d35e
Mike-chege <mikechege93@gmail.com>
2023-08-15 08:09:59 -0700

    Prints the firat argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-const process = require('process');
-let cmdarg = process.argv[2];
-if (cmdarg === undefined)
-{
-  console.log('No argument');
-}
-else
-{
-  console.log(arg);
-}
+// This script prints the first argument passed to it
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
