ff32a00943733b634dbe5c41b05b8ed03e3d2b58
Mike-chege <mikechege93@gmail.com>
2023-08-19 08:53:34 -0700

    Listing all the states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,6 @@ This script lists all states from the database hbtn_0e_0_usa
 
 import sys
 import MySQLdb
-import sys
-import MySQLdb
 
 if __name__ == "__main__":
         db = MySQLdb.connect(user=sys.argv[1],
