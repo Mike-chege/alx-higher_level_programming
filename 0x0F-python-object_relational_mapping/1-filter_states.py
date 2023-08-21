03bda9d559f7082aaf5064f0c61c9f2597d2ffef
Mike-chege <mikechege93@gmail.com>
2023-08-21 00:01:35 -0700

    Listing all the names starting with(N)

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script Lists states with a name starting with (N)
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    db = MySQLdb.connect(host='localhost',
+            user=username,
+            passwd=password, db=data,
+            port=3306)
+
+    # Implementing the cursor
+    crs = db.cursor()
+    # Listing all states starting with the name N
+    row_vals = crs.execute('''
+            SELECT * FROM states
+            WHERE states.name LIKE 'N%'
+            ORDER BY states.id
+            ''')
+    rows = crs.fetchall()
+    for row in rows:
+        print(row)
