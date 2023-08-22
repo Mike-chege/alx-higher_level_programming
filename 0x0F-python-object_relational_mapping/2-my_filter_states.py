8e512af3eda246129f3e33d31413be94aade980a
Mike-chege <mikechege93@gmail.com>
2023-08-22 01:38:58 -0700

    Created an SQL query using (format) which conatins the user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and displays all values in the states table
+"""
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+
+    # Implementing the cursor
+    crs = db.cursor()
+    crs.execute("SELECT * FROM states \
+            WHERE BINARY name ='{}'".format(sys.argv[4]))
+    # Implementing a list comprehension to print the output
+    # And also iterate through the values in states with a for loop
+    [print(state) for state in crs.fetchall()]
