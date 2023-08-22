6c3c79ca3194b69f0d4825afd0474fd326d4e410
Mike-chege <mikechege93@gmail.com>
2023-08-22 01:20:05 -0700

    Listing all the states that start with N Fixed typos

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQL.connect(
+    db = MySQLdb.connect(
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3])
