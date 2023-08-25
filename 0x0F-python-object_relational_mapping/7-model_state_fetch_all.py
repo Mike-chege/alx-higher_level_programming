9118b3bad11b144b8f5b206705eb8bf6b2ccb3fc
Mike-chege <mikechege93@gmail.com>
2023-08-25 01:55:45 -0700

    Used the pool_pre_ping argument to indicate whether pre-pinging is enabled or not

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,13 +7,14 @@ from the database hbtn_0e_6_usa
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
-from model_sate import state
+from model_sate import Base, State
 
 
 if __name__ == '__main__':
     engine = create_engine(
             "mysql+mysqldb://{}:@localhost/{}"
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
