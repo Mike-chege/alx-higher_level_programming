0904d59ca7a36366954633d739acc574b55c2e4f
Mike-chege <mikechege93@gmail.com>
2023-08-25 03:33:15 -0700

    Deleting all state objects that contain a letter a from the database/fixed typos

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
             .format(sys.argv[1], sys.argv[2], sys.argv[3]),
             pool_pre_ping=True)
 
-    Session = sessionmaker(bing=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     for s in session.query(State):
