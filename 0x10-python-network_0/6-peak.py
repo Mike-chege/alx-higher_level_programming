1337aba4c5b3b69612af3e3170feb44b8cd9667f
Mike-chege <mikechege93@gmail.com>
2023-08-31 04:39:20 -0700

    Finding the peak of unsorted array

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,17 +12,17 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
-    length = len(list_of_integers)
-    if length == 1:
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    elif length == 2:
+    elif size == 2:
         return max(list_of_integers)
 
-    mid = int(length / 2)
-    p = list_of_integers[mid]
-    if p > list_of_integers[mid - 1] and p > list_of_integers[mid + 1]:
-        return p
-    elif p < list_of_integers[mid - 1]:
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid + 1:])
