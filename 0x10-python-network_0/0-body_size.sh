3bff62b4776b50103adeeb807d377bbff5d74810
Mike-chege <mikechege93@gmail.com>
2023-08-31 02:44:52 -0700

    Sending a request the a URL and displying the size of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL,
-# And displays the size of the body of the response
-
-curl -s "$1" | wc -c
+# This script takes in a URL, sends a request to that URL
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
