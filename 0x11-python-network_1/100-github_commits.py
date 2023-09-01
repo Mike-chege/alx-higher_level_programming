caecce0f3879a7049f31118923ec0111d08889e0
Mike-chege <mikechege93@gmail.com>
2023-09-01 09:39:53 -0700

    Using GitHub Api to list the 10 most recent commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This script takes 2 arguments and lists 10 commits
+(from the most recent to oldest)
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2], sys.argv[1])
+    request = requests.get(url)
+    commits = request.json()
+    try:
+        for item in range(10):
+            print("{}: {}".format(
+                commits[item].get("sha"),
+                commits[item].get("commit").get("author").get("name")))
+    except ValueError:
+        pass
