40158a2607bcf216cedc496130eb5856d9cc1d5d
Mike-chege <mikechege93@gmail.com>
2023-09-01 07:09:20 -0700

    Fetching a URL to check the status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script  fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == '__main__':
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        result = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(result)))
+        print("\t- content: {}".format(result))
+        print("\t- utf8 content: {}".format(result.decode("utf-8")))
