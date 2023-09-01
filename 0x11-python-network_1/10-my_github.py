f70645f6177160063d39bf751bce702bfbbb0bf3
Mike-chege <mikechege93@gmail.com>
2023-09-01 09:16:00 -0700

    Using GitHub API to display the user id after passing the user info

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This script takes your GitHub credentials (username and password)
+And uses the GitHub API to display your id
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == '__main__':
+    data = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    request = requests.get("https://api.github.com/user", data)
+    print(request.json().get("id"))
