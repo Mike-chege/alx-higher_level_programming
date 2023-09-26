815c8e59d3865109afd4864d42989c6df523c4d3
Mike-chege <mikechege93@gmail.com>
2023-09-26 02:37:56 -0700

    Reading and printing the contents of a file / Fixed semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const process = require("process");
-const fs = require("fs")
+const process = require('process');
+const fs = require('fs');
 
-let filePath = process.argv[2];
+const filePath = process.argv[2];
 
-fs.readFile(filePath, "utf8", function (err, data) {
-	if (err != null) {
-		console.log(err);
-	} else {
-		process.stdout.write(data);
-	}
-})
+fs.readFile(filePath, 'utf8', function (err, data) {
+  if (err != null) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
