e0750e99b59edfee26c4b0c529aac6decfb48f6c
Mike-chege <mikechege93@gmail.com>
2023-09-26 11:59:14 -0700

    Fixed requests / typos / semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,39 +1,19 @@
 #!/usr/bin/node
 // This script prints the number of movies where
 // The character “Wedge Antilles” is present
+const process = require('process');
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const wedgeId = 18;
+const url = process.argv[2];
+let starwars;
 
-if (!apiUrl) {
-  console.error('Usage: node script.js <API_URL>');
-  process.exit(1);
-}
-
-// Make a request to the Star Wars API
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error:', response.statusCode, body);
-    return;
-  }
-
-  try {
-    const filmsData = JSON.parse(body).results;
-    const movies = filmsData.reduce((count, film) => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeId}/`)) {
-        return count + 1;
-      }
-      return count;
-    }, 0);
-
-    console.log(`${movies}`);
-  } catch (parseError) {
-    console.error('Error parsing response:', parseError);
+request(url, function (error, response, body) {
+  if (error == null) {
+    starwars = JSON.parse(body).results;
+    console.log(starwars.reduce((num, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? num + 1
+        : num;
+    }, 0));
   }
 });
