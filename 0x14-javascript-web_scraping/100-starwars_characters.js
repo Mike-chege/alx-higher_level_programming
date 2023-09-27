d693e5bcd3f5b8c06b9269b2af7df11ae818b3df
Mike-chege <mikechege93@gmail.com>
2023-09-27 05:24:49 -0700

    Printing all characters of a starwars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// This script prints all characters
+// Of a Star Wars movie
+const process = require('process');
+const request = require('request');
+const movieID = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieID;
+
+let characters;
+
+request(url, function (error, response, body) {
+  if (error == null) {
+    characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (error == null) {
+		console.log(JSON.parse(body).name);
+	}
+      });
+    });
+   }
+});
