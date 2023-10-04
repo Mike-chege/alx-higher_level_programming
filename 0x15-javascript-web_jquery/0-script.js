fa46641e9f12edacb60fc45764a0223a9601ca70
Mike-chege <mikechege93@gmail.com>
2023-10-04 12:02:30 -0700

    Changes the color of the header to red / fixed typos

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,1 +1,3 @@
-document.querySelector('header').style.color =' #FF0000';
+// This script updates the text color of
+// The <header> element to red (#FF0000)
+document.querySelector('header').style.color = '#FF0000';
