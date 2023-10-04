4a2037a4fcdc239f6daf4ce1e71effd6a55c0a14
Mike-chege <mikechege93@gmail.com>
2023-10-04 11:52:13 -0700

    Changing the color of an element

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// This script updates the text color
+// Of the <header> element to red (#FF0000)
+// Using document.querySelector to select the HTML tag
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
